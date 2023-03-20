30364a12e14da21f01758f6f46702ddc48f75915
Isreal <imcdonal@unca.edu>
2023-03-20 10:29:31 -0400

    completed lecture 16

--- lectures/lecture16/05-make-a-calculator/index.js
+++ lectures/lecture16/05-make-a-calculator/index.js
@@ -8,17 +8,68 @@ function addNumbers() {
     //    number (so that you can do math)!
     // 4. Add the two numbers together.
     // 5. Write the result to the #answer section
+    let firstNumber = document.querySelector('#num1').value;
+        firstNumber = Number (firstNumber);
+    let secondNumber = document.querySelector('#num2').value;
+        secondNumber = Number (secondNumber);
+    let sum = firstNumber + secondNumber;
+
+    document.querySelector('#answer').innerHTML = sum;
+    
 }
 
 function subtractNumbers() {
     // Same as above but subtract!
+    let firstNumber = document.querySelector('#num1').value;
+    firstNumber = Number (firstNumber);
+let secondNumber = document.querySelector('#num2').value;
+    secondNumber = Number (secondNumber);
+let sum = firstNumber - secondNumber;
+
+document.querySelector('#answer').innerHTML = sum;
+
 }
 
 function multiplyNumbers() {
     // Same as above but multiply!
+    let firstNumber = document.querySelector('#num1').value;
+    firstNumber = Number (firstNumber);
+let secondNumber = document.querySelector('#num2').value;
+    secondNumber = Number (secondNumber);
+let sum = firstNumber * secondNumber;
+
+document.querySelector('#answer').innerHTML = sum;
+
 }
 
 function divideNumbers() {
     // Same as above but divide!
+    let firstNumber = document.querySelector('#num1').value;
+    firstNumber = Number (firstNumber);
+let secondNumber = document.querySelector('#num2').value;
+    secondNumber = Number (secondNumber);
+let sum = firstNumber / secondNumber;
+
+document.querySelector('#answer').innerHTML = sum;
+
 }
 
+function expNumbers(){
+    let firstNumber = document.querySelector('#num1').value;
+    firstNumber = Number (firstNumber);
+let secondNumber = document.querySelector('#num2').value;
+    secondNumber = Number (secondNumber);
+let sum = firstNumber ** secondNumber;
+
+document.querySelector('#answer').innerHTML = sum;
+}
+
+function modNumber(){
+    let firstNumber = document.querySelector('#num1').value;
+    firstNumber = Number (firstNumber);
+let secondNumber = document.querySelector('#num2').value;
+    secondNumber = Number (secondNumber);
+let sum = firstNumber % secondNumber;
+
+document.querySelector('#answer').innerHTML = sum;
+}
